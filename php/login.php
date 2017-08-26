8bbdf21931b38a1b5e0d8a675667ddf1d5d250e4
sakiran <sakiran82@gmail.com>
2017-08-26 10:32:31 +0530

    sql injection

--- php/login.php
+++ php/login.php
@@ -3,10 +3,11 @@ include("config.php");
 session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // username and password received from loginform
-    $username  = mysqli_real_escape_string($dbconfig, $_POST['username']);
-    //$username  = $_POST['username'];
-    $password  = mysqli_real_escape_string($dbconfig, $_POST['password']);
-    //$password  = $_POST['password'];
+    //$username  = mysqli_real_escape_string($dbconfig, $_POST['username']);
+    $username  = $_POST['username'];
+    //$password  = mysqli_real_escape_string($dbconfig, $_POST['password']);
+    $password  = $_POST['password'];
+	//keerthana ' or username = 'keerthana
     $sql_query = "SELECT userid FROM usermanagement WHERE username='$username' and password='$password'";
     $result    = mysqli_query($dbconfig, $sql_query);
     $row       = mysqli_fetch_array($result, MYSQLI_ASSOC);
